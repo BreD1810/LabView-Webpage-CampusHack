12c464151bdf685036e6ba274a6a1c4f4b996aba
Joel Parkinson <32489040+JoelTParko@users.noreply.github.com>
2018-03-10 16:27:23 +0000

    Added color's to the table cells

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
     $.ajax({
+        type: "GET",
         url: "text.txt", 
-        success: function(){
+        success: function(data){
             alert("IT WORKED");
-        },
+            $("#test").text(data);
+            var lines = data.split("\r\n");
+            var myTable= "<table class=\"center\"><tr><th style='width: 100px; color: red; text-align: center'>Computer</th>";
+            myTable+= "<th style='width: 100px; color: red; text-align: center;'>Status</th>";
+            for (let i = 0; i < lines.length; i++) {
+                myTable+=  "<tr><td>ID: "+lines[i]+" </td>"
+                myTable+=  "<td>OFF</td></tr>"
+            }
+            myTable+="</table>";
+            document.write(myTable);
+        }, 
         error: function(){
             alert("fail");
+            $("#test").text("It Failed");
         }
     });
     
+    $("#Table td").each(function(){
+        if($(this).text()=="OFF"){
+            $(this).css('background-color','#ff0000')
+        }
+    });
+
 });
