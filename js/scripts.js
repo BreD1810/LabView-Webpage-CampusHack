c447640c7dc2b37c16c46508bfa2267f92e0784a
Joel Parkinson <32489040+JoelTParko@users.noreply.github.com>
2018-03-10 23:20:29 +0000

    Added persistant tabs between refresh Added admin page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-var labName = "Demo";
+var labName = "";
 
-$(document).ready(function(){
+var computerIDs = new Array();
+
     function getTable(){
         $.ajax({
             type: "GET",
@@ -20,17 +21,7 @@ $(document).ready(function(){
         });
       
     }
-    getTable();
-
-    $("#Table td").each(function(){
-        if($(this).text()=="0"){
-            $(this).css('background-color','#ff0000')
-        } else if($(this).text()=="1"){
-            $(this).css('background-color','#00ff00')
-        }
-    });
-
-});
+    getTable();    
 
 function createTable(computers){
     $("#Lab1Table").html("");
@@ -38,15 +29,24 @@ function createTable(computers){
     myTable+= "<th style='width: 100px; color: red; text-align: center;'>Status</th>";
     for (let i = 0; i < computers.length-1; i++) {
         var compStats = computers[i].split(",");
+        computerIDs[i] = compStats[1]
         myTable+=  "<tr><td>ID: "+compStats[0]+" </td>"
         myTable+=  "<td>"+compStats[1]+"</td></tr>"      
     }
     myTable+="</table>";
+/*
+    for (var i = 0, cell; cell = myTable.cells[i]; i++) {
+        alert($this.text)();
+        if($(this).text().indexOf("0") != -1){
+            alert($this.text());
+            $(this).css('background-color','#ff0000')
+        } else if($(this).text().indexOf("1") != -1){
+            $(this).css('background-color','#00ff00')
+        }
+    }
+    */
     $("#Lab1Table").append(myTable);
-    
-    
 }
-
 function openTab(evt, labName){
     var tablinks, tabcontent;
     tablinks = $(".tablink");
@@ -62,10 +62,29 @@ function openTab(evt, labName){
     
     document.getElementById(labName).style.display = "block";
     this.labName = labName;
-    evt.currentTarget.className += " active";  
-   
+    localStorage.setItem("labName", labName);
+    evt.currentTarget.className += " active";    
 }
-
-function getLabName(){
-    return labName;
+function addClient(){
+    var clientID = document.getElementById("clientID").value;
+    alert(clientID);
+    var clientName = document.getElementById("clientName").value;
+    var packet = ""+ clientID + " " + clientName;
+    $.ajax({
+        type: "POST",
+        url: "http://labview.me:8080/LabView/addclient",
+        data: packet,
+        success: function(){
+            alert("Connection successful");
+        },
+        error: function(){
+            alert("Fail");
+        }
+    });
+}
+function setActiveTab(){
+   
+    var labName = localStorage.getItem("labName");
+    document.getElementById(labName).style.display = "block";
+    localStorage.setItem("labName", "");
 }
