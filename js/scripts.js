d8375aec5b09266e0bdceaa66963cf7df7000a1c
Joel Parkinson <32489040+JoelTParko@users.noreply.github.com>
2018-03-10 17:54:57 +0000

    Changed appendChild to append. Line 39 of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ function createTable(){
         myTable+=  "<td>"+compParts[1]+"</td></tr>"
     }
     myTable+="</table>";
-    $("#Table").appendChild(myTable);
+    $("#Table").append(myTable);
 }
 
 function getLabName(){
